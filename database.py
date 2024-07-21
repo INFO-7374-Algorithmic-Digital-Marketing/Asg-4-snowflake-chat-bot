0a0c17b42daf75ef96aab32766aa0fdd9d573230
snehilaryan32 <aryan.s@northeastern.edu>
2024-07-21 00:17:18 -0400

    Added code for testing query templates

--- database.py
+++ database.py
@@ -16,19 +16,23 @@ engine = create_engine(
 
 def run_query(query):
     try:
-        with st.spinner("Running query..."):
-            connection = engine.connect()
-            connection.execute(text("USE SNOWFLAKE_SAMPLE_DATA.TPCDS_SF10TCL;"))
-            # Make parameterized query such that no DELETE / UPDATE queries can be run
-            query = text(query)
-            result = connection.execute(query).fetchall()
-            try:
-                result = pd.DataFrame(result)
-            except Exception as e:
-                print(e)            
-            finally:
-                return result
+        # with st.spinner("Running query..."):
+        connection = engine.connect()
+        print("Connection successful")
+        connection.execute(text("USE SNOWFLAKE_SAMPLE_DATA.TPCDS_SF10TCL;")) # Do we need this? DB is passed as connection parameter
+        # Make parameterized query such that no DELETE / UPDATE queries can be run
+        query = text(query)
+        result = connection.execute(query).fetchall()
+        try:
+            result = pd.DataFrame(result)
+        except Exception as e:
+            print(e)            
+        finally:
+            return result
     except Exception as e:
         return str(e)
     finally:
         connection.close()
+
+
+#############################################Query Template Functions####################################################
